4ec32bd48a310d9c616843b3ce2e0812a05e805d
felixlaii <felix.lai@hotmail.com>
2022-01-18 16:08:05 -0500

    completed like toggle

--- client/src/components/LikeButton/LikeButton.jsx
+++ client/src/components/LikeButton/LikeButton.jsx
@@ -2,20 +2,41 @@ import React, { Component } from 'react'
 
 export default class LikeButton extends Component {
     state = {
-        likes: 0
+        likes: 0,
+        updated: false
     }
 
-    incrementLike = () => {
-        let newLike = this.state.likes + 1
-            this.setState({
-                likes: newLike
-            }) 
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         likes: 0,
+    //         updated: false
+    //     }
+    // }
+
+    updateLikes = () => {
+        if(!this.state.updated) {
+            this.setState((prevState, props) => {
+                return {
+                    likes: prevState.likes + 1,
+                    updated: true
+                }
+            })
+        } else {
+            this.setState((prevState, props) => {
+                return {
+                    likes: prevState.likes - 1,
+                    updated: false
+                }
+            })
         }
+    }
+
     render() {
         return (
-            <div>
-             <button onClick={this.incrementLike}>heart: {this.state.likes}</button>
-
+            <div className="like-button">
+            <p className="like-button__icon" onClick={this.updateLikes}>Like</p>
+            <p className="like-button__counter">{this.state.likes}</p>
             </div>
         )
     }
--- client/src/components/SpaceItem/SpaceItem.jsx
+++ client/src/components/SpaceItem/SpaceItem.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import "./spaceItem.scss";
 import LikeButton from "../LikeButton/LikeButton";
 
-function SpaceItem({
-  title,
-  id,
-  img,
-  description,
-  date
-}) {
+function SpaceItem({ title, id, img, description, date }) {
   return (
     <div className="space-item">
-      <li>
-        <img className="space-item__image" src={img} alt="rover" />
-        <LikeButton />
-        <p className="space-item__description">{title}</p>
-        <p className="space-item__description">{description}</p>
-        <p className="space-item__description">{date}</p>
-      </li>
+      <div className="space-item__wrapper">
+        <div className="space-item__card">
+          <li className="space-item__list">
+          <p className="space-item__description">{title}</p>
+            <div className="space-item__picture">
+              <img className="space-item__image" src={img} alt="rover" />
+            </div>
+            <LikeButton />
+            <div className="space-item__details">
+              <p className="space-item__description">{date}</p>
+              <p className="space-item__description">{description}</p>
+            </div>
+          </li>
+        </div>
+      </div>
     </div>
   );
 }
